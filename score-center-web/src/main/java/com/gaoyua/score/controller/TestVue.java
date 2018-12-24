e2f73cc8793bad3c59e155ca6d112663d37c8b65
yaoyizhou1 <yauri.zhang@qunar.com>
2018-12-24 20:43:35 +0800

    初始化

---
+++ score-center-web/src/main/java/com/gaoyua/score/controller/TestVue.java
@@ -0,0 +1,15 @@
+package com.gaoyua.score.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2018/12/24 20:38
+ * @desc
+ */
+@Controller
+public class TestVue {
+
+}
