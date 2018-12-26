109172eeceb4389a0014c2e0464efcebbb44ee92
yaoyizhou1 <yauri.zhang@qunar.com>
2018-12-26 15:57:10 +0800

    初始化

--- score-center-web/src/main/java/com/gaoyua/score/controller/TestVue.java
+++ score-center-web/src/main/java/com/gaoyua/score/controller/TestVue.java
@@ -2,9 +2,12 @@ package com.gaoyua.score.controller;
 
 import com.gaoyua.score.common.ResultJson;
 import com.gaoyua.score.domain.User;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -20,23 +23,31 @@ import java.util.Map;
 @Controller
 public class TestVue {
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login")
     @ResponseBody
-    public ResultJson login(Model model, User user) {
+    public ResultJson login(@RequestBody User user) {
         // 查询商品列表
         ResultJson json = new ResultJson();
-        if(user.getPassWord().equals("123456")&&user.getUserName().equals("yaoyizhou1")){
+        if (user != null
+                && StringUtils.isNotBlank(user.getPassWord())
+                && StringUtils.isNotBlank(user.getUserName())
+                && user.getPassWord().equals("123456")
+                && user.getUserName().equals("yaoyizhou1")) {
             json.setData(true);
             json.setFlag(true);
         }
-        if(user.getPassWord().equals("123456")&&user.getUserName().equals("liguofan2")){
+        if (user != null
+                && StringUtils.isNotBlank(user.getPassWord())
+                && StringUtils.isNotBlank(user.getUserName())
+                && user.getPassWord().equals("123456")
+                && user.getUserName().equals("liguofan2")) {
             json.setData(true);
             json.setFlag(true);
         }
         return json;
     }
 
-    @RequestMapping(value = "/client")
+    @RequestMapping(value = "/user/list")
     @ResponseBody
     public ResultJson index() {
         ResultJson json = new ResultJson();
