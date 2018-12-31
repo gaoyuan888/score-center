d9f221d99d25bfd84db6be2be0dd22eb7d6f68f1
yaoyizhou1 <yauri.zhang@qunar.com>
2018-12-31 09:41:35 +0800

    初始化

---
+++ score-center-web/src/main/java/com/gaoyua/score/controller/ScoreCotroller.java
@@ -0,0 +1,20 @@
+package com.gaoyua.score.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2018/12/31 9:37
+ * @desc
+ */
+@Controller
+public class ScoreCotroller {
+
+    @RequestMapping("/")
+    public String index(){
+        return "/index";
+    }
+}
